cf60faf23190e78401a3d5f017584413101a2a82
John <aby.vecchio@gmail.com>
2023-07-12 21:11:18 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        new GameFrame();  // non hop bisogno di istanziare i'oggetto trazionale (Frame frame= new Frame)
+
+     }
+
+}
